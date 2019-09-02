df561dcaa176da194c25cb0cc50f3fda794c4e01
David Ferreira <contact@davidferreira-it.com>
2019-09-03 00:57:49 +0200

    progress

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var RoonApi = require("node-roon-api"),
     RoonApiTransport = require("node-roon-api-transport"),
     RoonApiBrowse    = require('node-roon-api-browse'),
     RoonApiStatus = require("node-roon-api-status"),
-    transport,
+    ApiTransport,
+    ApiBrowse,
     zones = [];
 
 // initialize Express.js
@@ -14,7 +15,7 @@ var express = require('express'),
 var roon = new RoonApi({
     extension_id:        "com.varunrandery.remote",
     display_name:        "Roon remote",
-    display_version:     "1.0.0",
+    display_version:     "1.0.1",
     publisher:           "Varun Randery",
     email:               "varun@randery.com",
     website:             "varunrandery.com",
@@ -22,9 +23,9 @@ var roon = new RoonApi({
     // handler: on connection with Roon core
     core_paired: function(core) {
         // connect to transport service
-        transport = core.services.RoonApiTransport;
-        browse = core.services.RoonApiBrowse;
-        transport.subscribe_zones(function(cmd, data) {
+        ApiTransport = core.services.RoonApiTransport;
+        ApiBrowse = core.services.RoonApiBrowse;
+        ApiTransport.subscribe_zones(function(cmd, data) {
             // on first connection to core, populate zones list and log zone names and ids
             if (cmd == "Subscribed") {
                 zones = data.zones;
@@ -41,7 +42,7 @@ var roon = new RoonApi({
                     }
                 } else if ("zones_removed" in data) {
                     for (var item in data.zones_removed) {
-                        zones.splice(zones.indexOf(transport.zone_by_zone_id(data.zones_removed[item])), 1);
+                        zones.splice(zones.indexOf(ApiTransport.zone_by_zone_id(data.zones_removed[item])), 1);
                     }
                 }
             // should not fire
@@ -64,7 +65,7 @@ var svc_status = new RoonApiStatus(roon);
 roon.init_services({
     // provide status of extension
     provided_services: [ svc_status ],
-    // require access to transport control
+    // require access to transport control and browse control
     required_services: [ RoonApiBrowse, RoonApiTransport ]
 });
 
@@ -90,77 +91,18 @@ function control(zone, cmd) {
     // commands which are handled with the same identifier
     var standards = ["play", "pause", "next", "previous", "playpause"];
     if (standards.indexOf(cmd) != -1) {
-        transport.control(zone, cmd);
+        ApiTransport.control(zone, cmd);
     } else if (cmd == "mute" || cmd == "unmute") {
         // transport.mute requires an output not a zone (takes the first output of the zone by default)
-        transport.mute(zone.outputs[0], cmd);
+        ApiTransport.mute(zone.outputs[0], cmd);
     } else if (cmd == "pause_all") {
-        transport.pause_all()
+        ApiTransport.pause_all()
     } else {
         console.log("! Command not supported");
         return false;
     }
     console.log("! Command executed");
     return false;
-}
-
-
-function refresh_browse(opts) {
-    opts = Object.assign({
-        hierarchy:          "browse",
-        zone_or_output_id:  v.current_zone_id,
-    }, opts);
-
-    core.services.RoonApiBrowse.browse(opts, (err, r) => {
-        if (err) { console.log(err, r); return; }
-
-        console.log(err, r);
-
-        if (r.action == 'list') {
-            v.$set("list", r.list);
-            v.$set("items", []);
-            var listoffset = r.list.display_offset > 0 ? r.list.display_offset : 0;
-            load_browse(listoffset);
-
-        } else if (r.action == 'message') {
-            alert((r.is_error ? "ERROR: " : "") + r.message);
-
-        } else if (r.action == 'replace_item') {
-            var i = 0;
-            var l = v.items;
-            while (i < l.length) {
-                if (l[i].item_key == opts.item_key) {
-                    l.splice(i, 1, r.item);
-                    break;
-                }
-                i++;
-            }
-            v.$set("items", l);
-
-        } else if (r.action == 'remove_item') {
-            var i = 0;
-            var l = v.items;
-            while (i < l.length) {
-                if (l[i].item_key == opts.item_key) {
-                    l.splice(i, 1);
-                    break;
-                }
-                i++;
-            }
-            v.$set("items", l);
-        }
-    });
-}
-
-function load_browse(listoffset) {
-    core.services.RoonApiBrowse.load({
-        hierarchy:          "browse",
-        offset:             listoffset,
-        set_display_offset: listoffset,
-    }, (err, r) => {
-        v.$set("listoffset", listoffset);
-        v.$set("items", r.items);
-    });
 }
 
 // handle incoming HTTP GET formatted like: http://<host>:<port>/api?command=<cmd>&zone=<zone>
@@ -182,7 +124,14 @@ app.get("/api", function(req, res) {
         } else {
             console.log("! Error: zone was not found");
         }
-    } else if (req.query.fetch) {
+    } else if (req.query.webradio && req.query.zone) {
+        console.log("Webradio!");
+        refresh_browse();
+        refresh_browse({ item_key: 1 });
+        control(zone, 'playpause');
+
+    }
+    else if (req.query.fetch) {
         var content = [];
         for (item in zones) {
             content.push(zones[item].display_name);
@@ -195,3 +144,62 @@ app.get("/api", function(req, res) {
 app.listen(3000, function() {
     console.log("* Listening on port 3000")
 });
+
+///////////////////////////////////////
+function refresh_browse(opts) {
+    opts = Object.assign({
+        hierarchy:          "browse",
+        zone_or_output_id:  "RME%20ADI-2%20DAC",
+    }, opts);
+
+    ApiBrowse.browse(opts, (err, r) => {
+        if (err) { console.log(err, r); return; }
+
+        console.log(err, r);
+
+        if (r.action == 'list') {
+            console.log($set("list", r.list));
+            console.log($set("items", []));
+            var listoffset = r.list.display_offset > 0 ? r.list.display_offset : 0;
+            load_browse(listoffset);
+
+        } else if (r.action == 'message') {
+            alert((r.is_error ? "ERROR: " : "") + r.message);
+
+        } else if (r.action == 'replace_item') {
+            var i = 0;
+            var l = v.items;
+            while (i < l.length) {
+                if (l[i].item_key == opts.item_key) {
+                    l.splice(i, 1, r.item);
+                    break;
+                }
+                i++;
+            }
+            console.log($set("items", l));
+
+        } else if (r.action == 'remove_item') {
+            var i = 0;
+            var l = v.items;
+            while (i < l.length) {
+                if (l[i].item_key == opts.item_key) {
+                    l.splice(i, 1);
+                    break;
+                }
+                i++;
+            }
+            console.log($set("items", l));
+        }
+    });
+}
+
+function load_browse(listoffset) {
+    ApiBrowse.load({
+        hierarchy:          "browse",
+        offset:             listoffset,
+        set_display_offset: listoffset,
+    }, (err, r) => {
+        console.log($set("listoffset", listoffset));
+        console.log($set("items", r.items));
+    });
+}
